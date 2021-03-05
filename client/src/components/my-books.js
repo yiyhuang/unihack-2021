e86a19b69eda9a64928540e40ff17ae5cac5c886
yiyhuang <oliviahyy1126@gmail.com>
2021-03-06 01:04:44 +1100

    Implement My Books (grid style book gallery)
    will add "flip to back to see description" function tmr

--- client/src/components/my-books.js
+++ client/src/components/my-books.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../../style/style.scss";
 
 export default class MyBooks extends Component {
   render() {
     return (
-      <div>
-      <h1>My Books</h1>
+      <div class="main">
+        <h1>My Books</h1>
+        <div class="row">
+          {/* foreach(book in books) */}
+          <div class="col">
+            <a class="bookCard" href="/books/:bookID">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg"
+                alt="book"
+                width="100%"
+              />
+              <h4>The Cat in the Hat</h4>
+            </a>
+          </div>
+          <div class="col">
+            <a class="bookCard" href="/books/:bookID">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/81TSB6zIIQL._AC_UL200_SR200,200_.jpg"
+                alt="book"
+                width="100%"
+              />
+              <h4>One Fish Two Fish Red Fish Blue Fish</h4>
+            </a>
+          </div>
+          <div class="col">
+            <a class="bookCard" href="/books/:bookID">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/81kdwZtIGbL._AC_UL200_SR200,200_.jpg"
+                alt="book"
+                width="100%"
+              />
+              <h4>Green Eggs and Ham</h4>
+            </a>
+          </div>
+          <div class="col">
+            <a class="bookCard" href="/books/:bookID">
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/81YsKZ7EuWL._AC_UL200_SR200,200_.jpg"
+                alt="book"
+                width="100%"
+              />
+              <h4>Oh, the Places You'll Go! </h4>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
