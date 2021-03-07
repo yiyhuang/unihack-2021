319eab24dd5d8b8503d8ce637d82aaaeed64d6ff
yiyhuang <oliviahyy1126@gmail.com>
2021-03-07 13:20:01 +1100

    added like variable in other-books for demonstration

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -12,7 +12,7 @@ export default class App extends Component {
         <h1 class="subtitle">
           Press the like button to start your book exchange journey!
         </h1>
-        <Example />
+        <BookCardStack />
       </div>
     );
   }
@@ -28,18 +28,24 @@ function HomeBookCard(props) {
   );
 }
 
-function Example() {
+function BookCardStack() {
   const [index, setIndex] = useState(0);
+  var currentBook = other_books[index];
 
-  const likeButton = (
-    <div class="has-text-centered p-6">
+  const likeButton =
+    currentBook.liked === true ? (
+      <button class="button is-large is-light is-focused is-danger">
+        <span class="icon">
+          <img src="../img/icon_heart.png" alt="like" />
+        </span>
+      </button>
+    ) : (
       <button class="button is-large">
         <span class="icon">
           <img src="../img/icon_heart.png" alt="like" />
         </span>
       </button>
-    </div>
-  );
+    );
 
   const prevButton =
     index === 0 ? null : (
@@ -65,12 +71,12 @@ function Example() {
         <div class="column has-text-right">{prevButton}</div>
         <center>
           <div class="column has-text-centered">
-            <HomeBookCard currentBook={other_books[index]} />
+            <HomeBookCard currentBook={currentBook} />
           </div>
         </center>
         <div class="column has-text-left">{nextButton}</div>
       </div>
-      {likeButton}
+      <div class="has-text-centered p-6">{likeButton}</div>
     </div>
   );
 }
--- client/src/data/other-books.js
+++ client/src/data/other-books.js
@@ -6,6 +6,7 @@ const other_books = [
       "https://images-na.ssl-images-amazon.com/images/I/71eJk7GtUOL._AC_UL200_SR200,200_.jpg",
     description:
       "A sweet tale rendered in swirly black calligraphy and watercolor. Childlike in its simplicity, its messages are universal",
+    liked: false,
   },
 
   {
@@ -15,6 +16,7 @@ const other_books = [
       "https://images-na.ssl-images-amazon.com/images/I/81yL8GKdrhL._AC_UL200_SR200,200_.jpg",
     description:
       "Love the colors and downright silliness. I still think of this book on occasions when I see something very unusual or odd that’s actually kind of funny.",
+    liked: true,
   },
 
   {
@@ -24,6 +26,7 @@ const other_books = [
       "https://images-na.ssl-images-amazon.com/images/I/81ZRrzqNmdL._AC_UL200_SR200,200_.jpg",
     description:
       "Still a fun read with important life lessons. A classic book with a modern message.",
+    liked: false,
   },
 
   {
@@ -33,6 +36,7 @@ const other_books = [
       "https://images-na.ssl-images-amazon.com/images/I/91jgrV6XiiL._AC_UL200_SR200,200_.jpg",
     description:
       "We love this story! In today’s climate of shining a light on social issues and working for justice for underrepresented communities, this tale is more timely than ever.",
+    liked: false,
   },
 
   {
@@ -42,6 +46,7 @@ const other_books = [
       "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
     description:
       "Cute book. Sweet story. Amelia Hapworth and Tim Warnes have created a cute and calming reading experience. ",
+    liked: false,
   },
 ];
 
